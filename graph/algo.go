f945af1c4fb7776c3afaacce4a76dd60a6c42975
Alexey Marfenkov <marfenkov@group-ib.ru>
2020-05-05 13:00:39 +0300

    add Bellman - Ford and negative cycles

--- graph/algo.go
+++ graph/algo.go
@@ -44,6 +44,64 @@ func GetShortestDAGPath(s string, g *Graph) *Path {
 	return p
 }
 
+func FindNegativeCycle(g *Graph, p *Path) []string {
+	cyclePathEnd := ""
+	for u, adj := range g.GetAdjList() {
+		for _, v := range adj {
+			if _, changed := p.Relax(u, v, g.GetWeight(u, v)); changed {
+				cyclePathEnd = v
+			}
+		}
+	}
+	if cyclePathEnd == "" {
+		return nil
+	}
+	v := cyclePathEnd
+	visited := make(map[string]bool)
+	visited[v] = true
+	var cycle []string
+	for {
+		v = p.Pred[v]
+		if visited[v] {
+			cycle = append(cycle, v)
+			pred := p.Pred[v]
+			for v != pred {
+				cycle = append(cycle, pred)
+				pred = p.Pred[pred]
+			}
+			break
+		}
+		visited[v] = true
+	}
+	revert(cycle)
+	return cycle
+}
+
+func revert(s []string) []string {
+	i := 0
+	j := len(s) - 1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+	return s
+}
+
+func BellmanFord(s string, g *Graph) *Path {
+	p := NewPath(s)
+
+	n := len(g.GetVertices())
+	for i := 0; i < n-1; i++ {
+		for u, adj := range g.GetAdjList() {
+			for _, v := range adj {
+				p.Relax(u, v, g.GetWeight(u, v))
+			}
+		}
+	}
+	return p
+}
+
 func Dijkstra(s string, g *Graph) *Path {
 	h := NewHeap()
 	p := NewPath(s)
--- main.go
+++ main.go
@@ -7,7 +7,37 @@ import (
 )
 
 func main() {
-	exampleDijkstra()
+	exampleBellmanFord()
+}
+
+func exampleBellmanFord() {
+	g := graph.NewGraph()
+	g.SetWeight("s", "t", 6)
+	g.SetWeight("s", "y", 7)
+	g.SetWeight("t", "x", 5)
+	g.SetWeight("t", "y", 8)
+	g.SetWeight("t", "z", -4)
+	g.SetWeight("x", "t", -2)
+	g.SetWeight("z", "x", 7)
+	g.SetWeight("z", "s", 2)
+	g.SetWeight("y", "x", -3)
+	g.SetWeight("y", "z", 9)
+
+	start := "s"
+	path := graph.BellmanFord(start, g)
+	for _, v := range g.GetVertices() {
+		if v != start {
+			path.Print(v)
+			fmt.Println()
+		}
+	}
+	fmt.Print("----------------------------\n")
+	g.SetWeight("t", "s", -4)
+
+	newPath := graph.BellmanFord(start, g)
+	cycle := graph.FindNegativeCycle(g, newPath)
+	fmt.Println(cycle)
+
 }
 
 func exampleDijkstra() {
