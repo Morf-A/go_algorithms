9bab83945cd0109710861a3a0e7ccd664f7c6073
Alexey Marfenkov <marfenkov@group-ib.ru>
2020-12-07 16:52:32 +0300

    lzw decoder

--- compression/example.go
+++ compression/example.go
@@ -8,6 +8,43 @@ import (
 	"strings"
 )
 
+func ExampleZLW() {
+	// bookBytes := []byte("ACAAGGTAGGAAAATGCGAAAGCTTAATTGCGGGA")
+	bookBytes := []byte("ABCDABC")
+	book := bytes.NewReader(bookBytes)
+
+	encoded := LZWEncode(book)
+
+	// res, err := ioutil.ReadAll(encoded)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(res)
+	// return
+
+	encodedCopy := &bytes.Buffer{}
+	decoded := LZWDecode(io.TeeReader(encoded, encodedCopy))
+
+	original, err := ioutil.ReadAll(decoded)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(original), string(bookBytes))
+	if bytes.Equal(original, bookBytes) {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not equal")
+	}
+	originalLen := len(original)
+	encodedLen := len(encodedCopy.Bytes())
+	fmt.Printf(
+		"%d -> %d %.2f%%\n",
+		originalLen,
+		encodedLen,
+		float64(encodedLen)*float64(100)/float64(originalLen),
+	)
+}
+
 func ExampleAdaptiveHuffman() {
 	bookBytes := []byte("ACAAGGTAGGAAAATGCGAAAGCTTAATTGCGGGA")
 	book := bytes.NewReader(bookBytes)
--- compression/lzw.go
+++ compression/lzw.go
@@ -5,12 +5,11 @@ import (
 	"container/list"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"io"
 )
 
 type LZWEncoder struct {
-	bitList   *list.List
+	byteList  *list.List
 	maxCode   uint32
 	seqLookUp map[string]uint32
 	buffer    *bufio.Reader
@@ -35,19 +34,19 @@ func (lzwe *LZWEncoder) Read(toFill []byte) (i int, err error) {
 }
 
 func (lzwe *LZWEncoder) nextEncodedByte() (byte, error) {
-	next := lzwe.bitList.Front()
+	next := lzwe.byteList.Front()
 	if next == nil {
 		bytes, err := lzwe.nextEncodedBytes()
 		if err != nil {
 			return 0, err
 		}
 		for _, b := range bytes {
-			lzwe.bitList.PushBack(b)
+			lzwe.byteList.PushBack(b)
 		}
-		next = lzwe.bitList.Front()
+		next = lzwe.byteList.Front()
 	}
 	res := next.Value.(byte)
-	lzwe.bitList.Remove(next)
+	lzwe.byteList.Remove(next)
 	return res, nil
 }
 
@@ -63,7 +62,6 @@ func (lzwe *LZWEncoder) nextEncodedBytes() ([]byte, error) {
 			if !ok {
 				return nil, errors.New("Can`t find code by string " + string(lzwe.maxSeq))
 			}
-			fmt.Println("encode", string(lzwe.maxSeq), code)
 			lzwe.maxSeq = nil
 			res := make([]byte, 4)
 			binary.BigEndian.PutUint32(res, code)
@@ -80,8 +78,6 @@ func (lzwe *LZWEncoder) nextEncodedBytes() ([]byte, error) {
 			if !ok {
 				return nil, errors.New("Can`t find code by string " + string(lzwe.maxSeq[:len(lzwe.maxSeq)-1]))
 			}
-			fmt.Println("encode", string(lzwe.maxSeq[:len(lzwe.maxSeq)-1]), code)
-			fmt.Println("add", string(lzwe.maxSeq), lzwe.maxCode)
 			lzwe.maxSeq = []byte{nextByte}
 			res := make([]byte, 4)
 			binary.BigEndian.PutUint32(res, code)
@@ -90,33 +86,102 @@ func (lzwe *LZWEncoder) nextEncodedBytes() ([]byte, error) {
 	}
 }
 
-func initSeq() (uint32, map[string]uint32) {
-	seqLookUp := make(map[string]uint32)
-	var i uint32 = 0
-	for i < 256 {
-		seqLookUp[string(i)] = i
-		i++
-	}
-	return i, seqLookUp
-}
-
 func LZWEncode(in io.Reader) io.Reader {
-	code, seqLookUp := initSeq()
+	seqLookUp := make(map[string]uint32)
+	var code uint32 = 0
+	for code < 256 {
+		seqLookUp[string(code)] = code
+		code++
+	}
 	return &LZWEncoder{
 		buffer:    bufio.NewReader(in),
 		maxCode:   code,
-		bitList:   list.New(),
+		byteList:  list.New(),
 		seqLookUp: seqLookUp,
 	}
 }
 
 type LZWDecoder struct {
+	byteList  *list.List
+	maxCode   uint32
+	seqLookUp map[string]uint32
+	sequences []string
+	buffer    *bufio.Reader
+	maxSeq    []byte
+	isEOF     bool
 }
 
-func (lzwd *LZWDecoder) Read(p []byte) (int, error) {
-	return 0, nil
+func (lzwd *LZWDecoder) Read(toFill []byte) (i int, err error) {
+	for i < len(toFill) {
+		var b byte
+		b, err = lzwd.nextDecodedByte()
+		if err != nil {
+			break
+		}
+		toFill[i] = b
+		i++
+	}
+	if i > 0 {
+		err = nil
+	}
+	return
+}
+
+func (lzwd *LZWDecoder) nextUint32() (uint32, error) {
+	intBytes := make([]byte, 4)
+	for i := 0; i < 4; i++ {
+		nextByte, err := lzwd.buffer.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		intBytes[i] = nextByte
+	}
+	return binary.BigEndian.Uint32(intBytes), nil
+}
+
+func (lzwd *LZWDecoder) nextDecodedBytes() ([]byte, error) {
+	nextCode, err := lzwd.nextUint32()
+	if err != nil {
+		return nil, err
+	}
+	if int(nextCode) > len(lzwd.sequences)-1 {
+		return []byte{0}, nil
+	}
+	seq := lzwd.sequences[nextCode]
+	return []byte(seq), nil
+}
+
+func (lzwd *LZWDecoder) nextDecodedByte() (byte, error) {
+	next := lzwd.byteList.Front()
+	if next == nil {
+		bytes, err := lzwd.nextDecodedBytes()
+		if err != nil {
+			return 0, err
+		}
+		for _, b := range bytes {
+			lzwd.byteList.PushBack(b)
+		}
+		next = lzwd.byteList.Front()
+	}
+	res := next.Value.(byte)
+	lzwd.byteList.Remove(next)
+	return res, nil
 }
 
 func LZWDecode(in io.Reader) io.Reader {
-	return &LZWDecoder{}
+	seqLookUp := make(map[string]uint32)
+	var sequences []string
+	var code uint32 = 0
+	for code < 256 {
+		seqLookUp[string(code)] = code
+		sequences = append(sequences, string(code))
+		code++
+	}
+	return &LZWDecoder{
+		buffer:    bufio.NewReader(in),
+		maxCode:   code,
+		byteList:  list.New(),
+		seqLookUp: seqLookUp,
+		sequences: sequences,
+	}
 }
